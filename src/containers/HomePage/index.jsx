1e04d8905fad497aecee73ad54b6bbcf8fc28933
Elian <eliancarlogm@gmail.com>
2022-06-20 23:42:18 -0430

    chores: change import Menuheader

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import Header from "../../components/Header";
+import MenuHeader from "../../components/MenuHeader";
 
 const HomePage = (props) => {
    return (
       <Layout>
-         <Header></Header>
+         <Header />
+         <MenuHeader />
       </Layout>
    );
 };
