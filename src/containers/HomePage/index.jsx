866f83f13e748346b4b87d879949370424579311
Elian <eliancarlogm@gmail.com>
2022-06-28 12:26:00 -0430

    refactor(Layout): delete the container component of bootstrap, and add Layout component into HomePage container

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import Header from "../../components/Header";
-import MenuHeader from "../../components/MenuHeader";
 
 const HomePage = (props) => {
    return (
-      <>
-         <Header />
-         <MenuHeader />
-      </>
+      <Layout>
+         <h1>Home page</h1>
+      </Layout>
    );
 };
 
