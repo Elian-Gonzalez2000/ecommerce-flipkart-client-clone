847270ef5e63960e3d9a7e5f2cbb70c9e8078834
Elian <eliancarlogm@gmail.com>
2022-07-01 11:48:53 -0430

    feat(containers/ProductListPage): Show the data of a specific product, the UI is divided in section depending of the price products

--- src/containers/ProductListPage/index.jsx
+++ src/containers/ProductListPage/index.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getProductsBySlug } from "../../actions";
 import Layout from "../../components/Layout";
 import "./styles.css";
 
 const ProductListPage = (props) => {
+   const product = useSelector((state) => state.product);
+   const [priceRange, setPriceRange] = useState({
+      under5k: 5000,
+      under10k: 10000,
+      under15k: 15000,
+      under20k: 20000,
+      under30k: 30000,
+   });
    const dispatch = useDispatch();
 
    useEffect(() => {
@@ -13,7 +21,38 @@ const ProductListPage = (props) => {
    }, []);
    return (
       <Layout>
-         <h1>Home page</h1>
+         {Object.keys(product.productsByPrice).map((key, index) => {
+            return (
+               <div className="card">
+                  <div className="card-header">
+                     <div>
+                        {props.match.params.slug} mobile under {priceRange[key]}
+                     </div>
+                  </div>
+                  <div style={{ display: "flex" }}>
+                     {product.productsByPrice[key].map((product) => (
+                        <div className="product-container">
+                           <div className="product-img-container">
+                              <img src="" alt="" />
+                           </div>
+                           <div className="product-info">
+                              <div style={{ margin: "5px 0" }}>
+                                 {product.name}
+                              </div>
+                              <div>
+                                 <span>4.3</span>&nbsp;
+                                 <span>3353</span>
+                              </div>
+                              <div className="productPrice">
+                                 {product.price}
+                              </div>
+                           </div>
+                        </div>
+                     ))}
+                  </div>
+               </div>
+            );
+         })}
       </Layout>
    );
 };
