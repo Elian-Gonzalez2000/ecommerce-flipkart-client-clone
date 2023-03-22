32138c1f2fb53bc2bfb8253d5fb23982c248c040
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-22 15:12:49 -0400

    feat(containers/ProductListPage): Delete the code for to be much readable and add a conditional render depending of the type page

--- src/containers/ProductListPage/index.jsx
+++ src/containers/ProductListPage/index.jsx
@@ -1,69 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
-import { getProductsBySlug } from "../../actions";
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 import Layout from "../../components/Layout";
-import { genericPublicUrl } from "../../urlConfig";
+import ProductStore from "./ProductStore/index.jsx";
+import ProductPage from "./ProductPage/index.jsx";
 import "./styles.css";
+import getParams from "../../utilities/getParams";
 
 const ProductListPage = (props) => {
-   const product = useSelector((state) => state.product);
-   const [priceRange, setPriceRange] = useState({
-      under5k: 5000,
-      under10k: 10000,
-      under15k: 15000,
-      under20k: 20000,
-      under30k: 30000,
-   });
-   const slug = useLocation().pathname.substring(1);
-   const dispatch = useDispatch();
-   console.log(useLocation().pathname.substring(1), product);
-   useEffect(() => {
-      const { match } = props;
-      dispatch(getProductsBySlug(slug));
-   }, []);
-   return (
-      <Layout>
-         {product.productsByPrice &&
-            Object.keys(product.productsByPrice).map((key, index) => {
-               return (
-                  <div className="card">
-                     <div className="card-header">
-                        <div>
-                           {slug} mobile under {priceRange[key]}
-                        </div>
-                     </div>
-                     <div style={{ display: "flex" }}>
-                        {product.productsByPrice[key].map((product) => (
-                           <div className="product-container">
-                              <div className="product-img-container">
-                                 <img
-                                    src={genericPublicUrl(
-                                       product.productPictures[0].img
-                                    )}
-                                    alt={`${product.name} image`}
-                                 />
-                              </div>
-                              <div className="product-info">
-                                 <div style={{ margin: "5px 0" }}>
-                                    {product.name}
-                                 </div>
-                                 <div>
-                                    <span>4.3</span>&nbsp;
-                                    <span>3353</span>
-                                 </div>
-                                 <div className="product-price">
-                                    {product.price}
-                                 </div>
-                              </div>
-                           </div>
-                        ))}
-                     </div>
-                  </div>
-               );
-            })}
-      </Layout>
-   );
+   const location = useLocation();
+   const renderProduct = () => {
+      const params = getParams(location.search);
+      console.log(location, params);
+      let content = null;
+      switch (params.type) {
+         case "store":
+            content = <ProductStore {...props} location={location} />;
+            break;
+         case "page":
+            content = <ProductPage {...props} location={location} />;
+            break;
+         default:
+            content = null;
+      }
+      return content;
+   };
+   return <Layout>{renderProduct()}</Layout>;
 };
 
 export default ProductListPage;
