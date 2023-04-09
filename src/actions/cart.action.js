db05e1c111f3411d6d666b3e940a5ddb709b7baa
Elian Gonzalez <eliancarlogm@gmail.com>
2023-04-08 22:18:46 -0400

    fix(actions/cart.action.js): Error with the variable name cart to cartitems in the conditional if

--- src/actions/cart.action.js
+++ src/actions/cart.action.js
@@ -1,11 +1,12 @@
 import axios from "../helpers/axios";
 import store from "../store";
+import { cartConstants } from "./constants";
 
 export const addToCart = (product) => {
    return async (dispatch) => {
       const { cartItems } = store.getState().cart;
       const quantity = cartItems[product._id]
-         ? parseInt(product[product._id].quantity + 1)
+         ? parseInt(cartItems[product._id].quantity + 1)
          : 1;
 
       cartItems[product._id] = {
@@ -13,9 +14,26 @@ export const addToCart = (product) => {
          quantity,
       };
 
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+
       dispatch({
          type: cartConstants.ADD_TO_CART,
          payload: { cartItems },
       });
+   };
+};
+
+export const updateToCart = () => {
+   return async (dispatch) => {
+      const cartItems = localStorage.getItem("cart")
+         ? JSON.parse(localStorage.getItem("cart"))
+         : null;
+
+      if (cartItems) {
+         dispatch({
+            type: cartConstants.ADD_TO_CART,
+            payload: { cartItems },
+         });
+      }
    };
 };
