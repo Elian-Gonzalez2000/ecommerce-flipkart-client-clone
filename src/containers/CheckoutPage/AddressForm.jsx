89d215f8ee78fc7186024dc71b75d52e2264a0c0
Elian Gonzalez <eliancarlogm@gmail.com>
2023-09-18 20:46:17 -0400

    refactor(containers/CheckoutPage/AddressForm.jsx): the component now recive the data of the address when the page need edit an address and update, almost fix an render error when Add a new address

--- src/containers/CheckoutPage/AddressForm.jsx
+++ src/containers/CheckoutPage/AddressForm.jsx
@@ -5,16 +5,34 @@ import { MaterialButton, MaterialInput } from "../../components/MaterialUI";
 
 const AddressForm = (props) => {
    const user = useSelector((state) => state.user);
-   const [name, setName] = useState("");
-   const [mobileNumber, setMobileNumber] = useState("");
-   const [pinCode, setPinCode] = useState("");
-   const [locality, setLocality] = useState("");
-   const [address, setAddress] = useState("");
-   const [cityDistrictTown, setCityDistrictTown] = useState("");
-   const [state, setState] = useState("");
-   const [landmark, setLandmark] = useState("");
-   const [alternatePhone, setAlternatePhone] = useState("");
-   const [addressType, setAddressType] = useState("");
+   const { initialData } = props;
+   const [id, setId] = useState(initialData ? initialData._id : "");
+   const [name, setName] = useState(initialData ? initialData.name : "");
+   const [mobileNumber, setMobileNumber] = useState(
+      initialData ? initialData.mobileNumber : ""
+   );
+   const [pinCode, setPinCode] = useState(
+      initialData ? initialData.pinCode : ""
+   );
+   const [locality, setLocality] = useState(
+      initialData ? initialData.locality : ""
+   );
+   const [address, setAddress] = useState(
+      initialData ? initialData.address : ""
+   );
+   const [cityDistrictTown, setCityDistrictTown] = useState(
+      initialData ? initialData.cityDistrictTown : ""
+   );
+   const [state, setState] = useState(initialData ? initialData.state : "");
+   const [landmark, setLandmark] = useState(
+      initialData ? initialData.landmark : ""
+   );
+   const [alternatePhone, setAlternatePhone] = useState(
+      initialData ? initialData.alternatePhone : ""
+   );
+   const [addressType, setAddressType] = useState(
+      initialData ? initialData.addressType : ""
+   );
    const [submitFlag, setSubmitFlag] = useState(false);
    const dispatch = useDispatch();
 
@@ -39,6 +57,9 @@ const AddressForm = (props) => {
          },
       };
       console.log(payload);
+      if (id) {
+         payload.address._id = id;
+      }
       dispatch(addAddress(payload));
       setSubmitFlag(true);
    };
@@ -47,8 +68,25 @@ const AddressForm = (props) => {
       console.log("address count: ", user.address);
       if (submitFlag) {
          console.log("where we are ", user);
-         const address = user.address.slice(user.address.length - 1)[0];
-         props.onSubmitForm(address);
+         let _address = {};
+         if (id) {
+            _address = {
+               _id: id,
+               name,
+               mobileNumber,
+               pinCode,
+               locality,
+               address,
+               cityDistrictTown,
+               state,
+               landmark,
+               alternatePhone,
+               addressType,
+            };
+         } else {
+            _address = user.address.slice(user.address.length - 1)[0];
+         }
+         props.onSubmitForm(_address);
       }
    }, [user.address]);
 
@@ -166,7 +204,7 @@ const AddressForm = (props) => {
    };
 
    if (props.withoutLayout) {
-      return <div>{renderAddressForm}</div>;
+      return <div>{renderAddressForm()}</div>;
    }
 
    return (
@@ -182,7 +220,9 @@ const AddressForm = (props) => {
                padding: "0 60px",
                paddingBottom: "20px",
             }}
-         ></div>
+         >
+            {renderAddressForm()}
+         </div>
       </div>
    );
 };
