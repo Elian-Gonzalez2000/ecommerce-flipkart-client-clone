749411edb3fa4da0b41b440c7cb7f19d87034034
Elian Gonzalez <eliancarlogm@gmail.com>
2023-09-24 00:11:28 -0400

    feat(src/containers/OrderPage/index.jsx): show the data of orders users

--- src/containers/OrderPage/index.jsx
+++ src/containers/OrderPage/index.jsx
@@ -1,24 +1,53 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import Card from "../../components/UI/Card";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getOrders } from "../../actions";
+import { genericPublicUrl } from "../../urlConfig";
+import "./style.css";
 
 const OrderPage = (props) => {
+   const user = useSelector((state) => state.user);
    const dispatch = useDispatch();
    useEffect(() => {
       dispatch(getOrders());
    }, []);
    return (
       <Layout>
-         <Card style={{ maxWidth: "1200px", margin: "5px auto" }}>
-            <div className="orderItemContainer">
-               <div>img</div>
-               <div>name</div>
-               <div>price</div>
-               <div>order Status</div>
-            </div>
-         </Card>
+         {console.log(user.orders)}
+         {user.orders.length > 0 &&
+            user.orders.map((order) => {
+               return order.items.map((item) => (
+                  <div className="order-item-container">
+                     <div
+                        style={{
+                           display: "flex",
+                           flex: "1",
+                           justifyContent: "space-between",
+                        }}
+                     >
+                        <picture
+                           style={{
+                              width: "80px",
+                              height: "80px",
+                              overflow: "hidden",
+                              textAlign: "center",
+                           }}
+                        >
+                           <img
+                              style={{ maxWidth: "80px", maxHeight: "80px" }}
+                              src={genericPublicUrl(
+                                 item.productId.productPictures[0].img
+                              )}
+                           />
+                        </picture>
+                        <p style={{ width: "300px" }}>{item.productId.name}</p>
+                        <p>{item.payablePrice}</p>
+                        <p>{order.paymentStatus}</p>
+                     </div>
+                  </div>
+               ));
+            })}
       </Layout>
    );
 };
