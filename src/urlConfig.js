3b06cf23144c3a4759deeaca9d8b8bdd416c02fb
Elian Gonzalez <eliancarlogm@gmail.com>
2024-01-12 21:33:30 -0400

    change the URL API

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-/* export const api = "https://flipkart-rest-server-jvxd.onrender.com/api"; */
-export const api = "http://localhost:3002/api";
+export const api = "https://flipkart-rest-server-jvxd.onrender.com/api";
+/* export const api = "http://localhost:3002/api"; */
 
 export const genericPublicUrl = (fileName) => {
    return `http://localhost:3002/public/${fileName}`;
